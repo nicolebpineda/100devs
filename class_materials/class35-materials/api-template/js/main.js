54b7437283b13175678c2b368e57f2b7614db525
Nicole B Pineda <nicolebpineda@gmail.com>
2023-05-03 21:33:36 -0600

    created dog picture generator using dog.ceo api

--- class_materials/class35-materials/api-template/js/main.js
+++ class_materials/class35-materials/api-template/js/main.js
@@ -1,1 +1,18 @@
 //Make an api request using async await
+
+document.querySelector('button').addEventListener('click', getACuteDogPhoto)
+
+async function getACuteDogPhoto(){
+    try {
+        const res = await fetch('https://dog.ceo/api/breed/schnauzer/giant/images/random')
+        if (!res.ok){
+            throw new Error(`HTTP error: ${response.status}`)
+        }
+        const data = await res.json()
+        document.querySelector('img').src = data.message
+        console.log(data.message)
+    }
+    catch (error){
+        console.error(`Could not get products: ${error}`)
+    }
+}
